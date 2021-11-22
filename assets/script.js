c08d63156bc22a2f6f4feb2f51b4b4a51d4c0c3f
chaudeveloper <ms_michanguyen@yahoo.com>
2021-11-22 03:07:44 -0500

    create time comparison to each row and display different color, adding text ability for description

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,145 @@
-var m = moment().format('dddd, MMMM Do');
+var m = moment().format('dddd, MMMM Do LT');
 $('#currentDay').text(m);
 
-var timenow = moment().format('h a');
-console.log (timenow)
+var nineam= moment({
+    h: 9,
+    s: 0
+})
+if (moment().isAfter(nineam)){
+    $(".9am").addClass("past")
+}
+else if ((moment().isSame(nineam)))
+{
+    $(".9am").addClass("present")}
+else {
+    $(".9am").addClass("future")
+}
+var tenam= moment({
+    h: 10,
+    s: 0
+})
+if (moment().isAfter(tenam)){
+    $(".10am").addClass("past")
+}
+else if ((moment().isSame(tenam)))
+{
+    $(".10am").addClass("present")}
+else {
+    $(".10am").addClass("future")
+}
+
+var elevenam= moment({
+    h: 11,
+    s: 0
+})
+if (moment().isAfter(elevenam)){
+    $(".11am").addClass("past")
+}
+else if ((moment().isSame(elevenam)))
+{
+    $(".11am").addClass("present")}
+else {
+    $(".11am").addClass("future")
+}
+
+var twelvepm = moment({
+    h: 12,
+    s: 0
+})
+if (moment().isAfter(twelvepm)){
+    $(".12pm").addClass("past")
+}
+else if ((moment().isSame(twelvepm)))
+{
+    $(".12pm").addClass("present")}
+else {
+    $(".12pm").addClass("future")
+}
+
+var onepm = moment({
+    h: 13,
+    s: 0
+})
+if (moment().isAfter(onepm)){
+    $(".1pm").addClass("past")
+}
+else if ((moment().isSame(onepm)))
+{
+    $(".1pm").addClass("present")}
+else {
+    $(".1pm").addClass("future")
+}
+
+var twopm = moment({
+    h: 14,
+    s: 0
+})
+if (moment().isAfter(twopm )){
+    $(".2pm").addClass("past")
+}
+else if ((moment().isSame(twopm )))
+{
+    $(".2pm").addClass("present")}
+else {
+    $(".2pm").addClass("future")
+}
+
+var threepm = moment({
+    h: 15,
+    s: 0
+})
+if (moment().isAfter(threepm)){
+    $(".3pm").addClass("past")
+}
+else if ((moment().isSame(threepm)))
+{
+    $(".3pm").addClass("present")}
+else {
+    $(".3pm").addClass("future")
+}
+
+var fourpm = moment({
+    h: 16,
+    s: 0
+})
+if (moment().isAfter(fourpm)){
+    $(".4pm").addClass("past")
+}
+else if ((moment().isSame(fourpm)))
+{
+    $(".4pm").addClass("present")}
+else {
+    $(".4pm").addClass("future")
+}
 
 
+var fivepm = moment({
+    h: 17,
+    s: 0
+})
+if (moment().isAfter(fivepm)){
+    $(".5pm").addClass("past")
+}
+else if ((moment().isSame(fivepm)))
+{
+    $(".5pm").addClass("present")}
+else {
+    $(".5pm").addClass("future")
+}
 
+// var textInput = $(".description").on("click", function(){
+//     $(".row").addClass("input-group")
+//     $(".description").addType("text")
+// })
 
-// if (timenow >= 9) {
-//     $(".9am").addClass("past")
-// }
-// else {
-//     $(".9am").addClass("future")
-// }
+$(".description").on("click", function(){
+    var text = $(this)
+    .text()
+    .trim();
 
-// if (timenow >10) {
-//     $(".10am").addClass("past")
-// }
-// else {
-//     $(".10am").addClass("future")
-// }
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
 
-// if (timenow >= 11) {
-//     $(".9am").addClass("past")
-// }
-// else {
-//     $(".9am").addClass("future")
-// }
+})
+
