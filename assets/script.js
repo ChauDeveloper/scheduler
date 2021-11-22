419f762c7ee84902bb94bd8e1159099225e56ed8
chaudeveloper <ms_michanguyen@yahoo.com>
2021-11-22 00:06:15 -0500

    adding time rows and current time show

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var m = moment().format('dddd, MMMM Do');
+$('#currentDay').text(m);
+
+var timenow = moment().format('h a');
+console.log (timenow)
+
+
+
+
+// if (timenow >= 9) {
+//     $(".9am").addClass("past")
+// }
+// else {
+//     $(".9am").addClass("future")
+// }
+
+// if (timenow >10) {
+//     $(".10am").addClass("past")
+// }
+// else {
+//     $(".10am").addClass("future")
+// }
+
+// if (timenow >= 11) {
+//     $(".9am").addClass("past")
+// }
+// else {
+//     $(".9am").addClass("future")
+// }
